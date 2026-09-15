374d5fc48c276cba95eea53cd37ee374cf8f6ffd
lomachenkogennady-design <_@example.com>
2026-09-15 08:48:37 +0300

    Replace contacts: SPb address, new phone/email/site

--- assets/index.es-C4jQARGD.js
+++ assets/index.es-C4jQARGD.js
@@ -1,4 +1,4 @@
-import { c as commonjsGlobal, _ as _typeof$1, g as getDefaultExportFromCjs } from "./index-t5GWMrH_.js";
+import { c as commonjsGlobal, _ as _typeof$1, g as getDefaultExportFromCjs } from "./index-fzP38N_Z.js";
 var es_promise = {};
 var es_promise_constructor = {};
 var globalThis_1;
@@ -10431,4 +10431,4 @@ export {
   vectorsAngle,
   vectorsRatio
 };
-//# sourceMappingURL=index.es-BHA-RX2r.js.map
+//# sourceMappingURL=index.es-C4jQARGD.js.map
